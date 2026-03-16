4fe28b9d4ae0ceb143ea3e950760e2327d79f147
essyassir2-code <essyassir2@gmail.com>
2026-03-16 10:06:30 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,106 +1,45 @@
-const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
+const { Client, GatewayIntentBits, EmbedBuilder, SlashCommandBuilder } = require("discord.js");
+const { REST, Routes } = require("discord.js");
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildPresences,
-    GatewayIntentBits.GuildVoiceStates,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
+  intents: [GatewayIntentBits.Guilds]
 });
 
-const CHANNEL_ID = "1482849156333830224";
+const commands = [
+  new SlashCommandBuilder()
+  .setName("send")
+  .setDescription("Send announcement")
+  .addStringOption(option =>
+    option.setName("text")
+    .setDescription("write your announcement")
+    .setRequired(true)
+  )
+].map(command => command.toJSON());
 
-client.once("ready", () => {
-  console.log("Bot is online");
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
-  const guild = client.guilds.cache.first();
-  const channel = client.channels.cache.get(CHANNEL_ID);
+(async () => {
+  await rest.put(
+    Routes.applicationCommands("1482839946736697488"),
+    { body: commands }
+  );
+})();
 
-  function sendStats(){
+client.on("interactionCreate", async interaction => {
 
-    const members = guild.memberCount;
+  if (!interaction.isChatInputCommand()) return;
 
-    const online = guild.members.cache.filter(
-      m => m.presence && m.presence.status !== "offline"
-    ).size;
+  if (interaction.commandName === "send") {
 
-    const voice = guild.members.cache.filter(
-      m => m.voice.channel
-    ).size;
-
-    const bots = guild.members.cache.filter(
-      m => m.user.bot
-    ).size;
-
-    const boosts = guild.premiumSubscriptionCount;
-
-    const embed = new EmbedBuilder()
-
-    .setColor("#5865F2")
-
-    .setTitle(`${guild.name} → Stats`)
-
-    .setDescription(`
-👥 **Members:** ${members}
-🟢 **Online:** ${online}
-🎤 **In Voice:** ${voice}
-🤖 **Bots:** ${bots}
-🚀 **Boosts:** ${boosts}
-`)
-
-    .setFooter({ text: "Stay Active & Enjoy Your Time" })
-    .setTimestamp();
-
-    channel.send({ embeds: [embed] });
-
-  }
-
-  sendStats();
-
-  setInterval(sendStats, 60000);
-
-});
-
-client.on("messageCreate", async (message) => {
-
-  if(message.author.bot) return;
-
-  if(message.content === "!donate"){
+    const text = interaction.options.getString("text");
 
     const embed = new EmbedBuilder()
+    .setDescription(text)
+    .setColor("Purple");
 
-    .setAuthor({
-      name: "PrimeManager",
-      iconURL: client.user.displayAvatarURL()
-    })
+    await interaction.reply({ content: "تم ارسال الاعلان", ephemeral: true });
 
-    .setTitle("💎 Server Donations")
-
-    .setDescription(`
-✨ **Premium Roles**
-Get your premium role.
-
-✨ **Premium Rooms**
-Claim your private voice room.
-
-💳 Supported methods:
-• PayPal
-• Bank Transfer
-• BTC
-`)
-
-    .setImage("https://media.tenor.com/m6oAJbC4YfAAAAAC/gojo.gif")
-
-    .setColor("Purple")
-
-    .setFooter({
-      text: "PrimeManager • Donations"
-    });
-
-    message.channel.send({ embeds:[embed] });
+    interaction.channel.send({ embeds:[embed] });
 
   }
 
